1670971950b118b4db19d85d7e306c2e70afc3a2
João Roberto <robertoo.jof@gmail.com>
2025-11-28 16:25:05 -0300

    adiciona repositório JsonPost para gerenciamento de posts a partir de um arquivo JSON

---
+++ simple_blog/src/repositories/post/jsonPost.repository.ts
@@ -0,0 +1,36 @@
+import { PostModel } from '@/models/post/post.model';
+import { PostRepository } from '@/repositories/post/post.repository';
+import { resolve } from 'path';
+import fs from 'fs';
+import { PostsSchema } from '@/models/post/post.schema';
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_PATH = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
+
+export class JsonPostRepository implements PostRepository {
+  private async _readFromDisk(): Promise<PostModel[]> {
+    const data = await fs.promises.readFile(JSON_POSTS_PATH, 'utf-8');
+    const json = JSON.parse(data);
+
+    const result = PostsSchema.safeParse(json);
+
+    if (!result.success) {
+      throw new Error('JSON inválido para PostModel');
+    }
+
+    return result.data.posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this._readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel | null> {
+    const posts = await this._readFromDisk();
+
+    const post = posts.find(post => post.id === id) || null;
+
+    return post;
+  }
+}
