8f9b6ab6eddc43281c289b3b3dd7d315fce71727
Roberto <robertoo.jof@gmail.com>
2025-11-30 14:55:43 -0300

    substitui SpinLoader por PostsList na página inicial

--- simple_blog/src/app/page.tsx
+++ simple_blog/src/app/page.tsx
@@ -1,9 +1,10 @@
-import SpinLoader from '@/components/SpinLoader';
+import PostsList from '@/components/PostsList';
 
 export default function Home() {
   return (
-    <div>
-      <SpinLoader className='min-h-40' />
+    <div className='container mx-auto p-4'>
+      <h1 className='text-3xl font-bold underline'>Posts</h1>
+      <PostsList />
     </div>
   );
 }
