50700e80ce03330f43c728f4d7a97617d6e22c89
Roberto <robertoo.jof@gmail.com>
2025-12-03 15:50:35 -0300

    atualiza a página de erro 404 para incluir um layout mais informativo e estilizado

--- simple_blog/src/app/not-found.tsx
+++ simple_blog/src/app/not-found.tsx
@@ -1,3 +1,19 @@
+import { cn } from '@/lib/utils';
+
 export default function NotFound() {
-  return <h1>Página não encontrada!</h1>;
+  return (
+    <div
+      className={cn(
+        'min-h-80 bg-slate-900 text-slate-100',
+        'mb-16 p-8 rounded-xl',
+        'flex items-center justify-center',
+        'text-center',
+      )}
+    >
+      <div>
+        <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
+        <p>Erro 404: A página que você está procurando não foi encontrada.</p>
+      </div>
+    </div>
+  );
 }
