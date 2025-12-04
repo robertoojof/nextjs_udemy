401ccc62196386aa2fb54c3d28afe3ded6f1bccb
Roberto <robertoo.jof@gmail.com>
2025-12-04 08:12:46 -0300

    adiciona função para gerar metadados da página de post com base no slug

--- simple_blog/src/app/post/[slug]/page.tsx
+++ simple_blog/src/app/post/[slug]/page.tsx
@@ -1,15 +1,27 @@
 import { findPostBySlugCached } from '@/lib/posts/queries';
 import { PostModel } from '@/models/post/post.model';
-import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post: PostModel = await findPostBySlugCached(slug);
+
+  return {
+    title: `${post.title}`,
+    description: `${post.excerpt}`,
+  };
+}
+
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
 
-  const post = await findPostBySlugCached(slug);
+  const post: PostModel = await findPostBySlugCached(slug);
 
   return (
     <div>
