8b9b579dced3107e1911f910a9b0884fc88b5fc2
Roberto <robertoo.jof@gmail.com>
2025-12-07 03:08:22 -0300

    comenta exemplo page para exemplos futuros e servir de exemplo o que já foi usado

--- simple_blog/src/app/exemplo/[id]/page.tsx
+++ simple_blog/src/app/exemplo/[id]/page.tsx
@@ -1,29 +1,37 @@
-import { formatHour } from '@/lib/utils';
-import { revalidateExampleAction } from '../../actions/revalidate.example';
+// 'use server';
 
-export const dynamic = 'force-static';
+// import { formatHourCached } from '@/lib/utils.example';
+// import { revalidateExampleAction } from '../../actions/revalidate.example';
 
-export default async function ExemploPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const hora = formatHour(new Date().toISOString());
-  const { id } = await params;
+// // export const dynamic = 'force-static';
 
-  return (
-    <div>
-      <h2> {hora}</h2>
+// export default async function ExemploPage({
+//   params,
+// }: {
+//   params: Promise<{ id: string }>;
+// }) {
+//   const { id } = await params;
 
-      <form action={revalidateExampleAction} className='py-16'>
-        <input type='hidden' name='path' defaultValue={`/exemplo/${id}`} />
-        <button
-          type='submit'
-          className='bg-amber-300  p-2 rounded hover:bg-amber-400 transition cursor-pointer'
-        >
-          Revalidate this page
-        </button>
-      </form>
-    </div>
-  );
+//   const hora = await formatHourCached();
+
+//   return (
+//     <div>
+//       <h1>Exemplo {id}</h1>
+//       <h2> {hora}</h2>
+
+//       <form action={revalidateExampleAction} className='py-16'>
+//         <input type='hidden' name='path' defaultValue={`/exemplo/${id}`} />
+//         <button
+//           type='submit'
+//           className='bg-amber-300  p-2 rounded hover:bg-amber-400 transition cursor-pointer'
+//         >
+//           Revalidate this page
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+export default function ExemploPage() {
+  return <div>Exemplo Page</div>;
 }
