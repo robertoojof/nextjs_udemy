335fcc1358a720917330b81bb52757ebd74267e6
Roberto <robertoo.jof@gmail.com>
2025-12-07 01:54:29 -0300

    refatora o componente Footer para armazenar o ano atual em uma constante

--- simple_blog/src/components/Footer/index.tsx
+++ simple_blog/src/components/Footer/index.tsx
@@ -1,10 +1,13 @@
+'use client';
 import Link from 'next/link';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className='pb-14 text-center text-sm text-gray-500'>
       <p>
-        &copy; {new Date().getFullYear()} My Simple Blog. All rights reserved -{' '}
+        &copy; {currentYear} My Simple Blog. All rights reserved -{' '}
         <Link href='/' className='underline'>
           Home
         </Link>
