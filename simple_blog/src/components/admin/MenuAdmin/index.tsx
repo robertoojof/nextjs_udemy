bbf40bc2f39d2d0b42b744a57d63d6a94db208b0
João Roberto <robertoo.jof@gmail.com>
2025-12-10 12:09:07 -0300

    refatora MenuAdmin para adicionar funcionalidade de abrir/fechar menu e melhora a estrutura dos botões de navegação

--- simple_blog/src/components/admin/MenuAdmin/index.tsx
+++ simple_blog/src/components/admin/MenuAdmin/index.tsx
@@ -1,15 +1,28 @@
+'use client';
+
 import { cn } from '@/lib/utils';
-import { FileTextIcon, HouseIcon } from 'lucide-react';
+import {
+  CircleXIcon,
+  FileTextIcon,
+  HouseIcon,
+  MenuIcon,
+  PlusIcon,
+} from 'lucide-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const navClasses = cn(
     'bg-slate-900 text-slate-100 rounded-lg',
-    'flex flex-col  mb-8',
+    'flex flex-col mb-8',
     'sm:flex-row sm:flex-wrap',
-    // 'h-10',
-    // 'overflow-hidden',
+    'transition-all duration-300 ease-in-out',
+    !isOpen ? 'max-h-10 overflow-hidden' : 'max-h-[300px]',
+    'sm:overflow-visible sm:h-auto',
   );
+
   const linkClasses = cn(
     '[&>svg]:w-4 [&>svg]:h-4 px-4',
     'flex items-center justify-start gap-2',
@@ -19,22 +32,53 @@ export function MenuAdmin() {
     'hover:scale-105',
   );
 
+  const openCloseBtnClasses = cn(
+    linkClasses,
+    'hover:scale-100 cursor-pointer',
+    'text-blue-200 italic',
+    'sm:hidden',
+  );
+
   return (
     <nav className={navClasses}>
-      <a href='/' target='_blank' className={linkClasses}>
-        <HouseIcon />
-        Home
-      </a>
+      <button
+        onClick={() => setIsOpen(prev => !prev)}
+        className={openCloseBtnClasses}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
 
-      <Link className={linkClasses} href='/admin/post'>
-        <FileTextIcon />
-        Posts
-      </Link>
+      <button onClick={() => setIsOpen(false)}>
+        <Link className={linkClasses} href='/' target='_blank'>
+          <HouseIcon />
+          Home
+        </Link>
+      </button>
 
-      <Link className={linkClasses} href='/admin/post'>
-        <FileTextIcon />
-        Posts
-      </Link>
+      <button onClick={() => setIsOpen(false)}>
+        <Link className={linkClasses} href='/admin/post'>
+          <FileTextIcon />
+          Posts
+        </Link>
+      </button>
+
+      <button onClick={() => setIsOpen(false)}>
+        <Link className={linkClasses} href='/admin/post/new'>
+          <PlusIcon />
+          Criar post
+        </Link>
+      </button>
     </nav>
   );
 }
