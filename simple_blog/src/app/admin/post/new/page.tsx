0594df190cc6e9299aee2fedb16269359e139441
Roberto <robertoo.jof@gmail.com>
2025-12-11 18:55:36 -0300

    add  componente MarkdownEditor  e integra no ManagePostForm(Admin)

--- simple_blog/src/app/admin/post/new/page.tsx
+++ simple_blog/src/app/admin/post/new/page.tsx
@@ -1,36 +1,10 @@
-import { InputText } from '@/components/InputText';
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='flex flex-col gap-6'>
-      <InputText
-        labelText='Nome'
-        placeholder='Digite seu nome'
-        type='password'
-      />
-      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        defaultValue='Olá mundo'
-      />
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        readOnly
-      />
-      <InputText
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        defaultValue='Olá mundo'
-        readOnly
-      />
-    </div>
+    <>
+      <h1>Criar post</h1>
+      <ManagePostForm />
+    </>
   );
 }
