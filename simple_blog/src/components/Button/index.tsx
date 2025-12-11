37997cdf1bdf49232a8e361ddcd8e8bfae5ed813
Roberto <robertoo.jof@gmail.com>
2025-12-11 18:03:43 -0300

    refatora componente Button para utilizar createVariantMap e melhora a tipagem dos props

--- simple_blog/src/components/Button/index.tsx
+++ simple_blog/src/components/Button/index.tsx
@@ -1,25 +1,31 @@
 import { cn } from '@/lib/utils';
 
-type ButtonVariants = 'default' | 'ghost' | 'danger';
-type ButtonSizes = 'sm' | 'md' | 'lg';
+function createVariantMap<
+  TVariants extends Record<string, string>,
+  TSizes extends Record<string, string>,
+>(variants: TVariants, sizes: TSizes) {
+  return {
+    variants,
+    sizes,
+    classes: ({
+      variant,
+      size,
+      className,
+    }: {
+      variant: keyof TVariants;
+      size: keyof TSizes;
+      className?: string;
+    }) => cn(variants[variant], sizes[size], className),
+  };
+}
 
-type ButtonProps = {
-  variant?: ButtonVariants;
-  size?: ButtonSizes;
-} & React.ComponentProps<'button'>;
-
-export function Button({
-  variant = 'default',
-  size = 'md',
-  ...props
-}: ButtonProps) {
-  const buttonVariants: Record<ButtonVariants, string> = {
+const buttonMap = createVariantMap(
+  {
     default: cn('bg-blue-600 hover:bg-blue-700 text-blue-100'),
     ghost: cn('bg-slate-300 hover:bg-slate-400 text-slate-950'),
     danger: cn('bg-red-600 hover:bg-red-700 text-red-100'),
-  };
-
-  const buttonSizes: Record<ButtonSizes, string> = {
+  },
+  {
     sm: cn(
       'text-xs/tight',
       'py-1',
@@ -41,17 +47,30 @@ export function Button({
       'rounded-lg',
       '[&_svg]:w-5 [&_svg]:h-5 gap-3',
     ),
-  };
+  },
+);
 
+type ButtonVariants = keyof typeof buttonMap.variants;
+type ButtonSizes = keyof typeof buttonMap.sizes;
+
+type ButtonProps = {
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
+} & React.ComponentProps<'button'>;
+
+export function Button({
+  variant = 'default',
+  size = 'md',
+  className,
+  ...props
+}: ButtonProps) {
   const buttonClasses = cn(
-    buttonVariants[variant],
-    buttonSizes[size],
+    buttonMap.classes({ variant, size, className }),
     'flex items-center justify-center cursor-pointer',
     'transition',
     'disabled:bg-slate-200',
     'disabled:text-slate-400',
     'disabled:cursor-not-allowed',
-    props.className,
   );
 
   return <button {...props} className={buttonClasses} />;
