c1acd2a1403236088811d53783f04d18bc723122
João Roberto <robertoo.jof@gmail.com>
2025-12-20 13:04:23 -0300

    feat(user): adiciona DTO e entidade de usuário com validações

---
+++ middle_level_blog/back_blog/src/user/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  Entity,
+  // PrimaryColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar', unique: true })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @Column({ default: false })
+  forceLogout: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  // One to Many <- Post
+}
