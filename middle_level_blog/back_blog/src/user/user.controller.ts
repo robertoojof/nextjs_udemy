df0a865e7e254e5c73e84ce63e1b68902046ace1
João Roberto <robertoo.jof@gmail.com>
2025-12-20 15:01:25 -0300

    feat(user): adiciona DTOs para atualização de usuário e senha, e implementa métodos no serviço de usuário

--- middle_level_blog/back_blog/src/user/user.controller.ts
+++ middle_level_blog/back_blog/src/user/user.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Patch, Req } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {}
+
+  /* @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async findOne(@Req() req: AuthenticatedRequest) {
+    const user = await this.userService.findOneByOrFail({ id: req.user.id });
+    return new UserResponseDto(user);
+  } */
+
+  /* @Post()
+  async create(@Body() dto: CreateUserDto) {
+    const user = await this.userService.create(dto);
+    return new UserResponseDto(user);
+  } */
+
+  /* @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  async update(@Req() req: AuthenticatedRequest, @Body() dto: UpdateUserDto) {
+    const user = await this.userService.update(req.user.id, dto);
+    return new UserResponseDto(user);
+  } */
+
+  /* @UseGuards(JwtAuthGuard)
+  @Patch('me/password')
+  async updatePassword(
+    @Req() req: AuthenticatedRequest,
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    const user = await this.userService.updatePassword(req.user.id, dto);
+    return new UserResponseDto(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('me')
+  async remove(@Req() req: AuthenticatedRequest) {
+    const user = await this.userService.remove(req.user.id);
+    return new UserResponseDto(user);
+  } */
 }
--- middle_level_blog/back_blog/src/user/user.module.ts
+++ middle_level_blog/back_blog/src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { CommonModule } from '@/common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), CommonModule],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
