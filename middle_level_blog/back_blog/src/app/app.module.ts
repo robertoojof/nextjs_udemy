f7e59ed9283f1fb0c33654bcf97b59fefd774747
João Roberto <robertoo.jof@gmail.com>
2025-12-20 00:43:19 -0300

    Adiciona módulo de usuário ao AppModule

--- middle_level_blog/back_blog/src/app/app.module.ts
+++ middle_level_blog/back_blog/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from 'src/post/post.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PostModule, AuthModule],
+  imports: [PostModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
