ce865e749fbef940d9f1765e5f49f0eb0e3fc4f1
João Roberto <robertoo.jof@gmail.com>
2025-12-20 15:33:05 -0300

    feat(upload): implement file upload functionality with multer, including validation and storage

--- middle_level_blog/back_blog/src/upload/upload.controller.ts
+++ middle_level_blog/back_blog/src/upload/upload.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage, limits, fileFilter } from './upload.config';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+
+  //TODO adicionar autenticação
+
+  @Post()
+  // @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file', { storage, limits, fileFilter }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadService.handleUpload(file);
+  }
 }
