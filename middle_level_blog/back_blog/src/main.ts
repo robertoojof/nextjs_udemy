0fd867584b685a97c3b7d6fa993da16b663ff0f8
João Roberto <robertoo.jof@gmail.com>
2025-12-19 23:37:14 -0300

    Cria estrutura inicial do backend com AppController, AppService e AppModule

--- middle_level_blog/back_blog/src/main.ts
+++ middle_level_blog/back_blog/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -11,8 +11,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api-docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 8000);
 }
-bootstrap();
+void bootstrap();
