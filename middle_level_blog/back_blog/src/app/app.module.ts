d2a647c32671ec178c364bdcead203c1cb8c0cf8
João Roberto <robertoo.jof@gmail.com>
2025-12-20 00:41:55 -0300

    Adiciona módulo, controlador e serviço de autenticação

--- middle_level_blog/back_blog/src/app/app.module.ts
+++ middle_level_blog/back_blog/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from 'src/post/post.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [PostModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
